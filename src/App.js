5b28528535e20d7549b3626fc3a6000bdcf10bff
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2024-04-21 13:32:02 +0530

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import dependencies
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+// routes
+import Router from "./routes";
+import { UserProvider } from "./UserContext";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <Router />
+    </UserProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { Button } from "@mui/material";
+import Logo from "./Logo";
+import "./styles/navbar.css";
+
+import { GoogleLogin, googleLogout } from "@moeindana/google-oauth";
+import { UserContext, UserUpdateContext } from "../UserContext";
+
+const Navbar = () => {
+    const user = useContext(UserContext);
+    const setUser = useContext(UserUpdateContext);
+
+    const logout = () => {
+        googleLogout();
+        setUser(null);
+    };
+
+    return (
+        <nav className="navbar">
+            <Logo name={true} />
+            {user ? (
+                <Button variant="outlined" color="warning" onClick={logout}>
+                    Logout
+                </Button>
+            ) : (
+                <GoogleLogin
+                    onSuccess={(res) => {
+                        setUser(res);
+                    }}
+                    onError={() => {
+                        console.log("Login Failed");
+                    }}
+                />
+            )}
+        </nav>
+    );
+};
+export default Navbar;
